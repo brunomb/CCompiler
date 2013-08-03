984db4f15eec4807730549785eaf7098f8822932
Matheus Sampaio <matheus1401@gmail.com>
2013-08-03 20:23:49 -0300

    Adicionado exemplos para testes.

---
+++ CCompiler/src/examplesCorrects/Ex9.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int binarySearch(int *vetor, int begin, int end, int key) {
+	int mid = (begin + end) / 2;
+
+	if (vetor[mid] == key) {
+		return mid;
+	}
+
+	if (begin == end) {
+		return -1;
+	} else {
+
+		if (key < vetor[mid]) {
+			return binarySearch(vetor, begin, mid, key);
+		} else {
+			return binarySearch(vetor, mid + 1, end, key);
+		}
+
+	}
+}
+
+
+int main() {
+
+	int v[10] = {11, 23, 37, 43, 53, 68, 75, 89, 97, 109};
+	int input;
+
+	while (1) {
+		scanf("%d", &input);
+
+		if (input == -1) {
+			break;
+		}
+
+		printf("%d", binarySearch(v, 0, 9, input));
+	}
+
+
+	return 0;
+}
---
+++ CCompiler/src/examplesNotCorrects/Ex4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+    int input;
+	scanf("%d", &input);
+
+	if (input % 3 == 0) {
+		printf(sim");
+	} else {
+		printf("nao");
+	}
+
+	return 0;
+}
