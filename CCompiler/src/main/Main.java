0acf1bf042a0db207c0699d5afc1c8ff4b039827
Matheus Sampaio <matheus1401@gmail.com>
2013-08-04 19:12:46 -0300

    Removido codigo comentado;

--- CCompiler/src/main/Main.java
+++ CCompiler/src/main/Main.java
@@ -1,43 +1,34 @@
 package main;
+
 import java.io.FileReader;
 
 import cup.parser;
 import flex.LexicalAnalysisC;
-
 public class Main {
-//	public static void main (String[] args) {
-//		try {
-//			LexicalAnalysisCalculator scanner = new LexicalAnalysisCalculator( new java.io.FileReader(args[0]) );
-//			parser p = new parser(scanner);
-//			System.out.println(p.parse());
-//		} catch (Exception e) {	
-//			System.out.println(e);
-//		}
-//	}
-	  public static void main(String argv[]) {
-		    
-		  LexicalAnalysisC s;
-		    int errors = 0;
-		    
-		    for (int i = 0; i < argv.length; i++) {
-		      try {
-		        System.out.println("Parsing ["+argv[i]+"]");
-		        s = new LexicalAnalysisC(new FileReader(argv[i]));
-		        parser p = new parser(s);
-		        p.parse();
-		        System.out.println("Number of semantics errors = " + errors + ".");
-		        if(errors == 0) {
-		                System.out.println("OK :)");
-		        }
-		        else {
-		                System.out.println("Error :(");
-		        }
-		      }
-		      catch (Exception e) {
-		        e.printStackTrace(System.out);
-		        System.exit(1);
-		      }
-		    }
-		  }
-}
 
+	public static void main(String argv[]) {
+
+		LexicalAnalysisC s;
+		int errors = 0;
+
+		for (int i = 0; i < argv.length; i++) {
+			try {
+				System.out.println("Parsing [" + argv[i] + "]");
+				s = new LexicalAnalysisC(new FileReader(argv[i]));
+				parser p = new parser(s);
+				p.parse();
+				System.out.println("Number of semantics errors = " + errors
+						+ ".");
+				if (errors == 0) {
+					System.out.println("OK :)");
+				} else {
+					System.out.println("Error :(");
+				}
+			} catch (Exception e) {
+				e.printStackTrace(System.out);
+				System.exit(1);
+			}
+		}
+	}
+	
+}
