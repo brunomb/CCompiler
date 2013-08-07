47f74e649dacef0fee51f5be36453cf230b93d3e
Matheus Sampaio <matheus1401@gmail.com>
2013-08-07 01:16:18 -0300

    Removido #includes e quebra de linha;

--- CCompiler/src/examplesCorrects/Ex1.c
+++ CCompiler/src/examplesCorrects/Ex1.c
@@ -1,1 +1,1 @@
-/* 3) Qual o valor das variaveis a, b, c, d, e, f apos a execucao do seguinte trecho de codigo: int x = 2, y = 4; int a, b, c, d, e, f; a = x & y; b = x | y; c = x ^ y; d = ~x; e = x << 3; f = x >> 1; */  #include <stdio.h>  void main() { 	int x, y, z; 	x = y = 10; 	z = ++x; 	x = -x; 	y++; 	x = x + y - (z--);      printf("%d", x); }  /* output: -11 */   
+/*1)Qual o valor das variáveis x, y e z após o seguinte trecho de código:*/ void main() { 	int x, y, z; 	x = y = 10; 	z = ++x; 	x = -x; 	y++; 	x = x + y - (z--);      printf("%d", x); }  /* output: -11 */   
--- CCompiler/src/examplesNotCorrects/Ex4.c
+++ CCompiler/src/examplesNotCorrects/Ex4.c
@@ -1,1 +1,1 @@
-#include <stdio.h>  int main() {      int input; 	scanf("%d", &input);  	if (input % 3 == 0) { 		printf(sim"); 	} else { 		printf("nao"); 	}  	return 0; }  
+int main() {      int input; 	scanf("%d", &input);  	if (input % 3 == 0) { 		printf(sim"); 	} else { 		printf("nao"); 	}  	return 0; }  
