691e58665e299c6d7b94fb333baded350be87532
Matheus Sampaio <matheus1401@gmail.com>
2013-08-07 00:28:03 -0300

    Exemplos alterados;

--- CCompiler/src/examplesNotCorrects/Ex7.c
+++ CCompiler/src/examplesNotCorrects/Ex7.c
@@ -1,1 +1,1 @@
-#include <stdio.h>  int sum(int array[], int len) { 	int i; 	int result = 0; 	for (i = 0; i < len; ++i) { 		result += array[i] 	}  	return result; }  void main() { 	int nums[] = {1,2,3,4,5,6,7,8,9,15}; 	printf("%d", sum(nums, 10)); } 
+#include <stdio.h>  int sum(int array[], int len) { 	int i; 	int result = 0; 	for (i = 0; i < len; ++i) { 		result += array[i] 	}  	return result; }  void main() { 	int nums[] = {1,2,3,4,5,6,7,8,9,15}; 	printf("%d", sum(nums, 10)); }  
