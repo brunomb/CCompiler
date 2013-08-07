944bfe576eac511c8c4efddd220ce006cca9582f
Matheus Sampaio <matheus1401@gmail.com>
2013-08-07 02:28:37 -0300

    Finalizando.

--- CCompiler/runJFlex&CUP.bat
+++ CCompiler/runJFlex&CUP.bat
@@ -2,10 +2,8 @@ SET PATHSRC=C:\Users\Matheus\git\CCompiler\CCompiler\src
 
 cd %PATHSRC%
 
-java -jar lib\java-cup-11a.jar -destdir %PATHSRC%\cup -package cup -expect 2300 %PATHSRC%\cup\C.cup
-
 java -jar lib\JFlex.jar %PATHSRC%\flex\C.flex
 
-java bin\main\Main.class %PATHSRC%\entrada.txt
+java -jar lib\java-cup-11a.jar -destdir %PATHSRC%\cup -expect 500 -package cup %PATHSRC%\cup\C.cup
 
 pause
--- CCompiler/src/main/Main.java
+++ CCompiler/src/main/Main.java
@@ -5,47 +5,42 @@ import java.io.FileReader;
 
 import cup.parser;
 import flex.LexicalAnalysisC;
+
 public class Main {
 
 	public static void main(String argv[]) {
 
 		LexicalAnalysisC s;
-		int errors = 0;
 
 		for (int i = 0; i < argv.length; i++) {
-			
+
 			File file = new File(argv[i]);
-			
+
 			if (!file.exists()) {
-				String filepath = System.getProperty("user.dir") + "\\" + argv[i];
+				String filepath = System.getProperty("user.dir") + "\\"
+						+ argv[i];
 				System.out.println(filepath);
 				file = new File(filepath);
-				
+
 				if (!file.exists()) {
 					System.out.println("Arquivo " + argv[i]
 							+ " não encontrado.");
 					continue;
 				}
 			}
-			
+
 			try {
 				System.out.println("Parsing [" + argv[i] + "]");
 				s = new LexicalAnalysisC(new FileReader(file));
 				parser p = new parser(s);
 				p.parse();
-				System.out.println("Number of errors = " + errors
-						+ ".");
-				if (errors == 0) {
-					System.out.println("OK :)");
-				} else {
-					System.out.println("Error :(");
-				}
+				System.out.println("OK :)");
+
 			} catch (Exception e) {
-				e.printStackTrace(System.out);
-				
-				System.exit(1);
+				System.err.print(e.getMessage());
 			}
+
 		}
 	}
-	
+
 }
