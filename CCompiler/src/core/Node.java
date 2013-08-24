f728a22cc8c0a7bdd4af4e8dc969299f1a8abb35
Matheus Sampaio <matheus1401@gmail.com>
2013-08-24 15:42:03 -0300

    Adicionando classe Node;

---
+++ CCompiler/src/core/Node.java
@@ -0,0 +1,43 @@
+package core;
+
+public class Node {
+	String lexema;
+	String type;
+	Node parent;
+	
+	public Node(String lexema, String type, Node parent) {
+		this.lexema = lexema;
+		this.type = type;
+		this.parent = parent;
+	}
+
+	public String getLexema() {
+		return lexema;
+	}
+
+	public void setLexema(String lexema) {
+		this.lexema = lexema;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	public Node getParent() {
+		return this.parent;
+	}
+	
+	public void setParent(Node parent) {
+		this.parent = parent;
+	}
+	
+	public String toString() {
+		return this.lexema + "(" + this.type + ")";
+	}
+	
+	
+}
